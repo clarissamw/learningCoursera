c9463eeb2f2a1d888f5ff6b248d2cf0571fb9fcf
clarissamw <cmartallin473@gmail.com>
2025-06-17 11:13:04 +0800

    adding the javadoc for the problem explanation

--- untitled/src/HelloGoodBye.java
+++ untitled/src/HelloGoodBye.java
@@ -1,5 +1,12 @@
 import java.util.Scanner;
 
+/* Write a program HelloGoodbye.java that takes two names as command-line arguments and prints hello
+ and goodbye messages as shown below
+ (with the names for the hello message in the same order as the command-line arguments
+ and with the names for the goodbye message in reverse order).
+ */
+
+
 public class HelloGoodBye {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
