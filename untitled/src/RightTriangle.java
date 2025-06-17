9828e8608b8f4fd98a6fe894cb254dccdf25c2fc
clarissamw <cmartallin473@gmail.com>
2025-06-17 15:54:45 +0800

    solving the problem without if statements, looping, and array

--- untitled/src/RightTriangle.java
+++ untitled/src/RightTriangle.java
@@ -16,20 +16,14 @@ public class RightTriangle {
         Scanner scanner = new Scanner(System.in);
         System.out.println("enter 3 digits of input separate with space");
 
-        String[] input = scanner.nextLine().split(" ");
-        int a = Integer.parseInt(input[0]);
-        int b = Integer.parseInt(input[1]);
-        int c = Integer.parseInt(input[2]);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
 
         isTwoSquaresOfTwoIntegersEqualTheThirdInteger(a, b, c);
     }
 
     public static void isTwoSquaresOfTwoIntegersEqualTheThirdInteger(int a, int b, int c) {
-        if(Math.pow(a,2) + Math.pow(b,2) == Math.pow(c,2)) {
-            System.out.println(true);
-        }
-        else {
-            System.out.println(false);
-        }
+        System.out.println(Math.pow(a,2) + Math.pow(b,2) == Math.pow(c,2));
     }
 }
