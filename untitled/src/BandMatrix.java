f8216383fd5071507b98662a57b9b336b40de874
clarissamw <cmartallin473@gmail.com>
2025-06-18 22:27:13 +0800

    BandMatrix with scanner

---
+++ untitled/src/BandMatrix.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class BandMatrix {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int size = scanner.nextInt();
+    int width = scanner.nextInt();
+//    int size = Integer.parseInt(args[0]);
+//    int width = Integer.parseInt(args[1]);
+
+    // for row
+    for(int i = 0; i < size; i++) {
+
+      // for column
+      for(int j = 0; j < size; j++) {
+
+        // this is the diagonal
+        int distance = Math.abs(i - j);
+
+        if(distance > width) {
+          System.out.print("0  ");
+        }
+        else {
+          System.out.print("*  ");
+        }
+      }
+
+      // after all column down, we go to the next row
+      System.out.println(" ");
+    }
+
+  }
+}
