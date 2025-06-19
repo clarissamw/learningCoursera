843a18b25a3fd4efab5ec03fadc61fc635faf845
clarissamw <cmartallin473@gmail.com>
2025-06-19 10:30:46 +0800

    random walk on coursera still with scanner to test the input and output

---
+++ untitled/src/RandomWalk.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class RandomWalk {
+  public static void main(String[] args) {
+//    int r = Integer.parseInt((args[0]));
+    Scanner scanner = new Scanner(System.in);
+    int r = scanner.nextInt();
+    int x = 0;
+    int y = 0;
+    int steps = 0;
+
+    System.out.println("(" + x + ", " + y + ")");
+
+    while (Math.abs(x) + Math.abs(y) != r) {
+      double random = Math.random();
+
+      if(random < 0.25) {
+        x--; // west
+      }
+
+      else if(random < 0.5) {
+        x++; // east
+      }
+
+      else if(random < 0.75) {
+        y++; //north
+      }
+
+      else {
+        y--; // south
+      }
+
+      System.out.println("(" + x + ", " + y + ")");
+      steps++;
+    }
+
+    System.out.println("steps = " + steps);
+  }
+}
