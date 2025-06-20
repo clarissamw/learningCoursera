7b43368b73adbf29e15a56daa4ea9ced2e7e88ad
clarissamw <cmartallin473@gmail.com>
2025-06-20 16:08:45 +0800

    solving coursera module 3 for discretedistribution problem.
    idea:
    1. we store the amount of elements that we want to print (the first index of the element)
    2. create the array with the size of the amount of elements that we want to print
    3. store the rest of the input as it's going to become the weight
    4. calculate the sum of all the weight. it's gonna be the range for later
    5. calculate the cumulative of all weight. this is to determine which range that the random later will fall under
    6. calculate the random number
    7. compare with the range, which range that this random number falls under. and print the index (0-9) depends on the length of the weight array

---
+++ untitled/src/DiscreteDistribution.java
@@ -0,0 +1,42 @@
+public class DiscreteDistribution {
+  public static void main(String[] args) {
+    int slot = Integer.parseInt(args[0]);
+    int[] slots = new int[slot];
+    int[] distribution = new int[args.length-1];
+    int sum = 0;
+
+    // read all the weights and store them into distribution array
+    for (int i = 1; i < args.length; i++) {
+      // we cast to int because the return of args[i] is a string
+      distribution[i-1] = Integer.parseInt(args[i]);
+
+      // calculate the sum of all the weights
+      sum += distribution[i-1];
+    }
+
+    // calculate the cumulative to implicitly know the range
+    int[] cumulative = new int[distribution.length];
+    cumulative[0] = distribution[0];
+
+    for (int i = 1; i < distribution.length; i++) {
+      cumulative[i] = cumulative[i-1] + distribution[i];
+    }
+
+
+
+    // random the number
+    for (int i = 0; i < slots.length; i++) {
+      int r = (int) (Math.random() * sum);
+
+      // find which index that the r falls under
+      for(int j = 0; j < cumulative.length; j++) {
+
+        // if the r is lesser than the value in the cumulative
+        if(r < cumulative[j]) {
+          System.out.println(j + " ");
+          break;
+        }
+      }
+    }
+  }
+}
