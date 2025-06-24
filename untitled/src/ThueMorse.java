86c0f176f12df403e316136cb7b6e3c47e1d29fd
clarissamw <cmartallin473@gmail.com>
2025-06-24 18:05:17 +0800

    deleting the scanner

--- untitled/src/ThueMorse.java
+++ untitled/src/ThueMorse.java
@@ -2,11 +2,7 @@ import java.util.Scanner;
 
 public class ThueMorse {
   public static void main(String[] args) {
-//    int input = Integer.parseInt(args[0]);
-//    int[] sequence = new int[input];
-
-    Scanner scanner = new Scanner(System.in);
-    int input = scanner.nextInt();
+    int input = Integer.parseInt(args[0]);
     int[] sequence = new int[input];
 
     // to get the first sequence based on the input
